c92cfb4c7f7e9950bf8670a75d8f0537beaa704a
Rehan Mansoori <90987707+rhn-ryan@users.noreply.github.com>
2023-05-21 02:48:43 +0530

    "added auth files"

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,38 +1,19 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
-
-    const newHotel = new Hotel(req.body)
-
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-        
-    } catch (error) {
-        res.status(500).json(err)
-    }
-})
+router.post("/", createHotel);
 //UPDATE
-router.put("/", async (req,res)=>{
-
-    const updatedHotel = new Hotel(req.body)
-
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-        
-    } catch (error) {
-        res.status(500).json(err)
-    }
-})
+router.put("/:id", updateHotel)
 //DELETE
-
+router.delete("/:id", deleteHotel)
 //GET
-
+router.get("/:id", getHotel)
 //GET ALL
+router.get("/", getHotels)
 
 export default router 
